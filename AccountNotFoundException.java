21fd89318c4e69ea3f07cdcdc043b81baf8fc42d
Anushka Singh <118196671+Its-anushka@users.noreply.github.com>
2026-01-14 10:23:01 +0530

    Delete AccountNotFoundException.java

--- AccountNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package OnlineBankingSystem;
-
-public class AccountNotFoundException extends Exception{
-    public AccountNotFoundException(){
-        super("Target account does not exist");
-    }
-}

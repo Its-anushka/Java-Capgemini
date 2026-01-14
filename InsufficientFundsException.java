af4136f17d010504884f622a64507face8107076
Anushka Singh <118196671+Its-anushka@users.noreply.github.com>
2026-01-14 10:22:40 +0530

    Delete InsufficientFundsException.java

--- InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package OnlineBankingSystem;
-
-public class InsufficientFundsException extends Exception{
-    public InsufficientFundsException(){
-        super("Source account does not have enough balance");
-    }
-}
